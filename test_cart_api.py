06da46f04f8c80e55b0b9e48ac49ba9cde25559f
taurus5650 <taurus_5650@hotmail.com>
2025-06-02 22:12:09 +0800

    test_with_ai

--- test_cart_api.py
+++ test_cart_api.py
@@ -41,3 +41,27 @@ class TestCase:
             ]
         },
     }
+
+    @allure.feature('test_with_ai')
+    def test_with_ai(self):
+        test_cases_str = self.open_ai_client.ai_generate_test_cases(
+            method=self.PROMPT['method'],
+            api_path=self.PROMPT['api_path'],
+            request_sample=self.PROMPT['request_sample'],
+            response_sample=self.PROMPT['response_sample'],
+        )
+        test_cases = json.loads(test_cases_str)
+
+        for case in test_cases:
+            input_data = case['input']
+            expected_status = case['expected']['status_code']
+            expected_response = case['expected']['response']
+
+            resp = self.api.post_cart(
+                cartId=input_data['cartId'],
+                userId=input_data['userId'],
+                products=input_data['products']
+            )
+
+            assert resp.status_code == expected_status
+            assert resp.json() == expected_response
