7e63523cea26b236297e4a748388ebd69b395ac4
taurus5650 <taurus_5650@hotmail.com>
2025-06-02 22:18:59 +0800

    user_prompt_content

--- open_ai.py
+++ open_ai.py
@@ -1,15 +1,32 @@
 from openai import OpenAI
+import textwrap
+
 
 class OpenAIClient:
     def __init__(self, api_key="xxxx"):
         self.client = OpenAI(api_key=api_key)
 
-    def generate_api_test_cases(self, method: str, api_path: str, request_sample: dict, response_sample: dict):
+    def generate_api_test_cases(self, method: str, api_path: str, request_sample: dict, response_sample: dict, resp_err_code: str = None):
+        user_prompt_content = textwrap.dedent(
+            """
+            Please according the API sample to generate test case:
+            method: {method}
+            api path: {api_path}
+            request sample: {request_sample}
+            response sample: {request_sample}
+            response error code (if have): {resp_err_code}
+            """
+        ).strip()
+
         prompt = [
-            {"role": "system",
-             "content": "You're a Senior Software QA Engineer, will in charge designing test case."},
-            {"role": "user",
-             "content": f"Please according the API sample generate test case: \nmethod: {method}\napi_path: {api_path}\nrequest_sample: {request_sample}\nresponse_sample: {response_sample}"}
+            {
+                "role": "system",
+                "content": "You're a Senior Software QA Engineer, will in charge designing test cases."
+            },
+            {
+                "role": "user",
+                "content": user_prompt_content
+            }
         ]
         response = self.client.chat.completions.create(
             model='gpt-4.1-mini',
